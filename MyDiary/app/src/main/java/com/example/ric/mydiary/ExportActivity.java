1c14391b2b693296a2d094e8b5bfd829cbfa1a5e
Obelixx <Obelixx@users.noreply.github.com>
2016-01-18 03:12:32 +0200

    added ExportActivity.java to export to calendar

--- MyDiary/app/src/main/java/com/example/ric/mydiary/ExportActivity.java
+++ MyDiary/app/src/main/java/com/example/ric/mydiary/ExportActivity.java
@@ -1,29 +1,79 @@
 package com.example.ric.mydiary;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ric.mydiary.Database.Event;
+import com.example.ric.mydiary.Database.EventsDataSource;
+import com.example.ric.mydiary.HelperClasses.DateSetter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ExportActivity extends AppCompatActivity {
+    private EditText inputDate;
+    private DateSetter date;
+    private EventsDataSource mydb;
+    ArrayList<Event> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        inputDate = (EditText) findViewById(R.id.edit_date_export);
+        date = new DateSetter(this, inputDate);
+
+        mydb = new EventsDataSource(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                list = mydb.getEventsByDate(date.getChosenDate());
+
+                for (Event ev: list) {
+                    InsertInCalendar(ev.getTitle(),ev.getDescription(),ev.getPlace(),ev.getDateTime());
+                }
             }
         });
     }
 
+
+    private void InsertInCalendar(String title,String description, String place, Date date){
+        Intent calIntent = new Intent(Intent.ACTION_INSERT);
+        calIntent.setType("vnd.android.cursor.item/event");
+        calIntent.setData(CalendarContract.Events.CONTENT_URI);
+        calIntent.putExtra(CalendarContract.Events.TITLE, title);
+        calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, place);
+        calIntent.putExtra(CalendarContract.Events.DESCRIPTION, description);
+
+        GregorianCalendar calDate = new GregorianCalendar();
+        calDate.setTime(date);
+
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                calDate.getTimeInMillis());
+
+        calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                calDate.getTimeInMillis());
+
+        startActivity(calIntent);
+    }
 }
